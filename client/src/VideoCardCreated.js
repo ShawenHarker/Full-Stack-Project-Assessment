8686bca26b01d4f22d9bded6e8e56da206888867
Shawen Aldred Harker <harkershawen@gmail.com>
2021-08-05 23:42:36 +0200

    Level 100
    buttons was created, all click events must be created.

--- client/src/VideoCardCreated.js
+++ client/src/VideoCardCreated.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Data from "./Data/exampleresponse.json";
 import "./App.css";
 
 const VideoCardCreated = () => {
+  let [countPlusOne, setCountPlusOne] = useState();
+  let [countMinusOne, setCountMinusOne] = useState();
+
+  // const ratingPlusOne = () => {
+  //   setCountPlusOne((prevCountPlusOne) => prevCountPlusOne + 1);
+  // };
+
   return (
     <div>
       <div>
@@ -10,13 +17,26 @@ const VideoCardCreated = () => {
           <div key={index} className="container card card-display">
             <iframe
               src={data.url}
-              title="YouTube video player"
+              title={data.title}
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
-            />
+            ></iframe>
             <h4 className="card-title">{data.title}</h4>
-            <h6 className="card-text">{data.rating}</h6>
+            <h6 className="card-text">
+              <button
+                className="btn"
+                onClick={() =>
+                  setCountPlusOne((prevCountPlusOne) => prevCountPlusOne + 1)
+                }
+              >
+                <span class="material-icons thumb">thumb_up</span>
+              </button>
+              {data.rating}
+              <button className="btn">
+                <span class="material-icons thumb">thumb_down</span>
+              </button>
+            </h6>
           </div>
         ))}
       </div>
